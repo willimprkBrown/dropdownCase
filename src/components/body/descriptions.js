d4a9feb3f25a15743c3e82ba51907b518ad3b353
William Park <william_c_park@brown.edu>
2025-02-23 13:41:17 -0500

    try

---
+++ src/components/body/descriptions.js
@@ -0,0 +1,12 @@
+export const docsCaptions = [
+
+    '1. Initial dropdown menu opened by clicking',
+    '2. Hovering over a dropdown menu item to expand a submenu',
+    '3. Hovering over a dropdown menu item after opening dropdown menu with keyboard shortcut'
+]
+
+export const notepadCaptions = [
+    '1. Initial dropdown menu opened by clicking',
+    '2. Hovering over a dropdown menu item',
+    '3. Opening dropdown menu with keyboard shortcut'
+]
